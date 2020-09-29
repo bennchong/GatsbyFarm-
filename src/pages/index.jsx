1e092a186366b1a50206070242f804d2fb81eca7
Benn <xyers01@hotmail.com>
2020-09-29 17:07:32 +0800

    Cover page using graphql. Installed relevant plugins, gatsby source, sharp and transformer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
-import Layout from "../components/Layout"
-import View from "../components/View"
-import Status from "../components/Status"
 import Login from "../components/Login"
 import { makeStyles } from "@material-ui/core/styles";
 import {Typography} from "@material-ui/core";
+import BackgroundImage from 'gatsby-background-image'
+import { useStaticQuery, graphql} from "gatsby";
 
-const coverImage = require('../assets/coverLanding.jpg')
 const useStyles = makeStyles({
     cover: {
         backgroundSize: "cover",
-        backgroundImage: `url(${coverImage})`,
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
@@ -22,12 +18,26 @@ const useStyles = makeStyles({
 
 const Index = () => {
     const classes = useStyles();
-
+    const data = useStaticQuery(graphql`
+        query{
+            file(relativePath: {eq: "coverLanding.jpg"}) {
+              childImageSharp {
+                fluid {
+                  aspectRatio
+                  base64
+                  sizes
+                  src
+                  srcSet
+                }
+              }
+            }
+          }
+    `)
     return (
-        <div className={classes.cover}>
-            <Typography variant={"h1"} gutterBottom> HydroHDB </Typography>
-            <Login />
-        </div>
+            <BackgroundImage fluid={data.file.childImageSharp.fluid} alt="A Plant Picture" className={classes.cover}>
+                <Typography variant={"h1"} gutterBottom> HydroHDB </Typography>
+                <Login />
+            </BackgroundImage>
         )
 }
 
