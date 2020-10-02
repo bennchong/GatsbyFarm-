722569fb3947e773c8d4a79567694bbf16cfa237
Benjamin Chong <xyers@hotmail.com>
2020-10-02 15:22:18 +0800

    Commented out test

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,33 +1,32 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import View from "../components/View";
-import { useDocument } from 'react-firebase-hooks/firestore';
+import { useDocument } from "react-firebase-hooks/firestore";
 import useFirebase from "../utils/useFirebase";
 
 const Dashboard = () => {
-    const firebase = useFirebase();
-    useEffect(() => {
-        if (!firebase) return;
-        return firebase.auth().onAuthStateChanged((user) => {
-            console.log('User:', user);
-        });
-    }, [firebase]);
-    console.log(firebase)
-    const [value, loading, error] = useDocument(
-        firebase.firestore().doc('data/prototype'),
-        {
-            snapshotListenOptions: { includeMetadataChanges: true },
-        }
-    );
-
-    return (
-        <View>
-            <p>
-                {error && <strong>Error: {JSON.stringify(error)}</strong>}
-                {loading && <span>Document: Loading...</span>}
-                {value && <span>Document: {JSON.stringify(value.data())}</span>}
-            </p>
-        </View>
-    );
+  // const firebase = useFirebase();
+  // useEffect(() => {
+  //   if (!firebase) return;
+  //   return firebase.auth().onAuthStateChanged((user) => {
+  //     console.log("User:", user);
+  //   });
+  // }, [firebase]);
+  // console.log(firebase);
+  // const [value, loading, error] = useDocument(
+  //   firebase.firestore().doc("data/prototype"),
+  //   {
+  //     snapshotListenOptions: { includeMetadataChanges: true },
+  //   }
+  // );
+  return (
+    <View>
+      <p>
+        {/* {error && <strong>Error: {JSON.stringify(error)}</strong>}
+        {loading && <span>Document: Loading...</span>}
+        {value && <span>Document: {JSON.stringify(value.data())}</span>} */}
+      </p>
+    </View>
+  );
 };
 
 export default Dashboard;
