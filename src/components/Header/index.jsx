b4370755a2ee248dcec1c8372c205e90c4c38211
Benjamin Chong <xyers@hotmail.com>
2020-10-02 15:36:16 +0800

    moved profile to sidebar, adjusted logout button to suit smaller screen

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,62 +1,59 @@
-import React, {useState} from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react";
+import { Link } from "gatsby";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import {Typography} from "@material-ui/core";
-import {logout} from "../../utils/auth";
-import {navigate} from "@reach/router";
-import {useFirebase} from "gatsby-plugin-firebase";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { Typography } from "@material-ui/core";
+import { logout } from "../../utils/auth";
+import { navigate } from "@reach/router";
+import { useFirebase } from "gatsby-plugin-firebase";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 const useStyles = makeStyles({
-    headerContainer:{
-        display: "flex",
-        width: "100%",
-        height: "60px",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    headerTitle: {
-        width: "80%"
-    },
-    headerNav: {
-        display: "flex",
-        width: "20%",
-        justifyContent: 'space-around'
-    }
-})
+  headerContainer: {
+    display: "flex",
+    width: "100%",
+    height: "60px",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerTitle: {
+    width: "90%",
+  },
+  headerNav: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+});
 
 const Header = () => {
-    const classes = useStyles();
-    const [firebase, setFirebase] = useState();
+  const classes = useStyles();
+  const [firebase, setFirebase] = useState();
 
-    useFirebase(fb => {
-        setFirebase(fb);
-    }, [])
-    const handleClick = () => {
-        logout(firebase).then(() => navigate(`/`))
-    }
+  useFirebase((fb) => {
+    setFirebase(fb);
+  }, []);
+  const handleClick = () => {
+    logout(firebase).then(() => navigate(`/`));
+  };
 
-    return (
-        <div className={classes.headerContainer}>
-            <div className={classes.headerTitle}>
-                <Typography variant="h6"> HDB HYDRO </Typography>
-            </div>
-            <List className={classes.headerNav}>
-                <ListItem onClick={() => navigate(`/`) }>
-                    <ListItemText primary=" Profile " />
-                    <ListItemIcon> <AccountCircleIcon style={{ color: "white"}}/> </ListItemIcon>
-                </ListItem>
-                <ListItem onClick={handleClick}>
-                    <ListItemText primary=" Logout " />
-                    <ListItemIcon > <ExitToAppIcon style={{ color: "white"}}/> </ListItemIcon>
-                </ListItem>
-            </List>
-        </div>
-    )
-}
-export default Header
+  return (
+    <div className={classes.headerContainer}>
+      <div className={classes.headerTitle}>
+        <Typography variant="h6"> HDB HYDRO </Typography>
+      </div>
+      <List className={classes.headerNav}>
+        <ListItem onClick={handleClick}>
+          <ListItemText primary=" Logout " />
+          <ListItemIcon>
+            {" "}
+            <ExitToAppIcon style={{ color: "white" }} />{" "}
+          </ListItemIcon>
+        </ListItem>
+      </List>
+    </div>
+  );
+};
+export default Header;
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,179 +1,207 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import LinkedCameraIcon from '@material-ui/icons/LinkedCamera';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
-import { Link } from "gatsby"
+import React from "react";
+import { Helmet } from "react-helmet";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import LinkedCameraIcon from "@material-ui/icons/LinkedCamera";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import { Link } from "gatsby";
 
-import Header from "../Header"
+import Header from "../Header";
 
 // Global styles and component-specific styles.
-import "./global.css"
+import "./global.css";
 
 const drawerWidth = 200;
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    hide: {
-        display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-        justifyContent: 'flex-end',
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: -drawerWidth,
-    },
-    contentShift: {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-    },
-    activeLink: {
-        color: "red",
-        textDecoration: "underline",
-    }
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  activeLink: {
+    color: "red",
+    textDecoration: "underline",
+  },
 }));
 
 const Layout = ({ children }) => {
-    const classes = useStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-    return(
-        <div className={classes.root}>
-            <Helmet title="Gatsby Simple Firebase Authentication" />
-            <CssBaseline />
-            <AppBar position="fixed" className={clsx(classes.appBar, {[classes.appBarShift]: open,})}>
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        className={clsx(classes.menuButton, open && classes.hide)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Header />
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                className={classes.drawer}
-                variant="persistent"
-                anchor="left"
-                open={open}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-            >
-                <div className={classes.drawerHeader}>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                    </IconButton>
-                </div>
-                <Divider />
-                <List>
-                    <Link to="/app/dashboard" activeClassName={classes.activeLink}>
-                        <ListItem button>
-                            <ListItemIcon><DashboardIcon/></ListItemIcon>
-                            <ListItemText primary={"Dashboard"} />
-                        </ListItem>
-                    </Link>
-                    <Link to="/app/liveview" activeClassName={classes.activeLink}>
-                        <ListItem button>
-                            <ListItemIcon><LinkedCameraIcon/></ListItemIcon>
-                            <ListItemText primary={"Live View"} />
-                        </ListItem></Link>
-                    </List>
-                <Divider />
-                <List>
-                    <Link to="/app/plantwiki" activeClassName={classes.activeLink}>
-                        <ListItem button>
-                            <ListItemIcon><MenuBookIcon/></ListItemIcon>
-                            <ListItemText primary={"Plant Wiki"} />
-                        </ListItem>
-                    </Link>
-                    <Link to="/app/community" activeClassName={classes.activeLink}>
-                        <ListItem button>
-                            <ListItemIcon><SupervisedUserCircleIcon/></ListItemIcon>
-                            <ListItemText primary={"Community"} />
-                        </ListItem>
-                    </Link>
-                </List>
-            </Drawer>
-            <main
-                className={clsx(classes.content, {
-                    [classes.contentShift]: open,
-                })}
-            >
-                {children}
-            </main>
+  return (
+    <div className={classes.root}>
+      <Helmet title="Gatsby Simple Firebase Authentication" />
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, { [classes.appBarShift]: open })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Header />
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
         </div>
-    )
-}
+        <Divider />
+        <List>
+          <Link to="/app/dashboard" activeClassName={classes.activeLink}>
+            <ListItem button>
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Dashboard"} />
+            </ListItem>
+          </Link>
+          <Link to="/app/dailyview" activeClassName={classes.activeLink}>
+            <ListItem button>
+              <ListItemIcon>
+                <LinkedCameraIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Daily View"} />
+            </ListItem>
+          </Link>
+        </List>
+        <Divider />
+        <List>
+          <Link to="/app/plantwiki" activeClassName={classes.activeLink}>
+            <ListItem button>
+              <ListItemIcon>
+                <MenuBookIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Plant Wiki"} />
+            </ListItem>
+          </Link>
+          <Link to="/app/community" activeClassName={classes.activeLink}>
+            <ListItem button>
+              <ListItemIcon>
+                <SupervisedUserCircleIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Community"} />
+            </ListItem>
+          </Link>
+        </List>
+        <Divider />
+        <List>
+          <Link to="/app/profile" activeClassName={classes.activeLink}>
+            <ListItem button>
+              <ListItemIcon>
+                <AccountCircleIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Profile"} />
+            </ListItem>
+          </Link>
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        {children}
+      </main>
+    </div>
+  );
+};
 // <Header />
-export default Layout
+export default Layout;
