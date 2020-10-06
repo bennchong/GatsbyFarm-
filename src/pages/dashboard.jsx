24114edfe91b6def91829de665e2c3b1855f465f
Benn <xyers01@hotmail.com>
2020-10-06 11:40:45 +0800

    Updated firebase, and using realtimeDB now instead of firestore

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,29 +2,32 @@ import React, {useEffect} from 'react';
 import View from "../components/View";
 import { useDocument } from 'react-firebase-hooks/firestore';
 import useFirebase from "../utils/useFirebase";
+import { useObject } from 'react-firebase-hooks/database';
 
 const Dashboard = () => {
     const firebase = useFirebase();
-    useEffect(() => {
-        if (!firebase) return;
-        return firebase.auth().onAuthStateChanged((user) => {
-            console.log('User:', user);
-        });
-    }, [firebase]);
-    console.log(firebase)
-    const [value, loading, error] = useDocument(
-        firebase.firestore().doc('data/prototype'),
-        {
-            snapshotListenOptions: { includeMetadataChanges: true },
-        }
-    );
+    const [value, loading, error] = useObject(firebase.database().ref('sensors'));
+
+    // useEffect(() => {
+    //     if (!firebase) return;
+    //     return firebase.auth().onAuthStateChanged((user) => {
+    //         console.log('User:', user);
+    //     });
+    // }, [firebase]);
+    // console.log(firebase)
+    // const [value, loading, error] = useDocument(
+    //     firebase.firestore().doc('data/prototype'),
+    //     {
+    //         snapshotListenOptions: { includeMetadataChanges: true },
+    //     }
+    // );
 
     return (
         <View>
             <p>
                 {error && <strong>Error: {JSON.stringify(error)}</strong>}
                 {loading && <span>Document: Loading...</span>}
-                {value && <span>Document: {JSON.stringify(value.data())}</span>}
+                {value && <span>Document: {JSON.stringify(value)}</span>}
             </p>
         </View>
     );
